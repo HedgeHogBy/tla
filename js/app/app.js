9b05e4d273a63d62f2bf5aebeb9a84262dc4be6a
HedgeHogBy <stankevich.e@gmail.com>
2012-09-25 01:52:52 +0300

    front page

--- js/app/app.js
+++ js/app/app.js
@@ -6,8 +6,30 @@ window.App = {
 	pages: {},
 	
 	initialize : function() {
+        var locationMinsk = new google.maps.LatLng(53.90564543790776, 27.554806518554642);//(долгота, широта)
+
 		Backbone.history.start();
-	}
+        this.markersArr = [];
+
+        this.mapOptions = {
+            zoom: 12,
+            center: locationMinsk,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+
+        this.geocoder = new google.maps.Geocoder();
+	},
+
+    mapInit: function(mapContainer, mapOptions) {
+        this.map = new google.maps.Map(mapContainer, mapOptions);
+    },
+
+    directionsInit: function(map, panel){
+        this.directionsService = new google.maps.DirectionsService();
+        this.directionDisplay = new google.maps.DirectionsRenderer();
+        this.directionsDisplay.setMap(map);
+        this.directionsDisplay.setPanel(panel);
+    }
 }
 
 
--- js/app/views/pages/front-page.js
+++ js/app/views/pages/front-page.js
@@ -1,13 +1,16 @@
 App.MainPageFront = Backbone.View.extend({
 
 	el: $("#front"),
+    $map: $("#map"),
+    $directionsPanel: $("#directions"),
 	
     events: {
 
     },
 
     initialize : function() {
-
+        this.mapInit(this.$map.get(0), App.mapOptions);
+        this.directionsInit(this.$map.get(0), this.$directionsPanel.get(0));
     },
 
     render : function() {
